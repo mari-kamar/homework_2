b942dcda98f124192ec4113dc53c31ef3ee6831d
marina <engalychevamg@pik.ru>
2020-04-18 18:02:01 +0300

    Second homework from lesson 14/04/2020

---
+++ junit/src/test/java/BrowserTest.java
@@ -0,0 +1,41 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BrowserTest {
+    private WebDriver driver;
+    private WebdriverFactory factory = new WebdriverFactory();
+    private Logger logger = LogManager.getLogger(BrowserTest.class);
+    String browser = System.getProperty("browser");
+
+
+    @Before
+    public void setupDriver() {
+        if (browser == null) {
+            browser = "chrome";
+        } else {
+            browser = browser.toLowerCase();
+        };
+        driver = factory.create(browser, "options");
+        logger.info(browser + " Driver is setup");
+    }
+
+    @Test
+    public void openPage() {
+        driver.get("https://otus.ru");
+        logger.info("Page is open in " + browser + " browser");
+    }
+
+    @After
+    public void closePage() {
+        if (driver != null) {
+            driver.quit();
+            logger.info(browser + " browser is closed");
+        }
+    }
+}
